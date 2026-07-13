a34343fe85ca9c977b219b22dcad7b7f78922755
zeraX23 <officialjiyora@gmail.com>
2026-07-14 00:11:39 +0700

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'scanner-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './1000145444.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
